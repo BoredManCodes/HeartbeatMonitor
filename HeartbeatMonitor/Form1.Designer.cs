52e09e04815e44c349dea195f2c64501c850947b
Trent Buckley <78893529+BoredManCodes@users.noreply.github.com>
2022-02-26 07:31:15 +1100

    Initial commit

---
+++ HeartbeatMonitor/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace HeartbeatMonitor
+{
+    partial class Configuration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Configuration));
+            this.addressInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.intervalInput = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.logTimer = new System.Windows.Forms.Timer(this.components);
+            this.logDisplay = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.heartbeatTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // addressInput
+            // 
+            this.addressInput.Location = new System.Drawing.Point(116, 6);
+            this.addressInput.Name = "addressInput";
+            this.addressInput.Size = new System.Drawing.Size(100, 20);
+            this.addressInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Heartbeat Address:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Interval in Seconds:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // intervalInput
+            // 
+            this.intervalInput.Location = new System.Drawing.Point(116, 35);
+            this.intervalInput.Name = "intervalInput";
+            this.intervalInput.Size = new System.Drawing.Size(100, 20);
+            this.intervalInput.TabIndex = 2;
+            this.intervalInput.Text = "300";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(116, 61);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save Config";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Log Output:";
+            // 
+            // logTimer
+            // 
+            this.logTimer.Interval = 1000;
+            this.logTimer.Tick += new System.EventHandler(this.logTimer_Tick);
+            // 
+            // logDisplay
+            // 
+            this.logDisplay.Location = new System.Drawing.Point(13, 102);
+            this.logDisplay.Multiline = true;
+            this.logDisplay.Name = "logDisplay";
+            this.logDisplay.ReadOnly = true;
+            this.logDisplay.Size = new System.Drawing.Size(201, 221);
+            this.logDisplay.TabIndex = 7;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "HeartbeatMonitor";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.NotifyIcon_Click);
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.NotifyIcon_DoubleClick);
+            // 
+            // heartbeatTimer
+            // 
+            this.heartbeatTimer.Tick += new System.EventHandler(this.heartbeatTimer_Tick);
+            // 
+            // Configuration
+            // 
+            this.AcceptButton = this.saveButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 335);
+            this.Controls.Add(this.logDisplay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.intervalInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addressInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Configuration";
+            this.Text = "Config";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_FormShown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox addressInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox intervalInput;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer logTimer;
+        private System.Windows.Forms.TextBox logDisplay;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer heartbeatTimer;
+    }
+}
+
---
+++ HeartbeatMonitor/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeartbeatMonitor
+{
+    public partial class Configuration : Form
+    {
+        public Configuration()
+        {
+            InitializeComponent();
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            logTimer.Start();
+        }
+
+        private void logTimer_Tick(object sender, EventArgs e)
+        {
+            logDisplay.Text = File.ReadAllText("log.txt");
+        }
+        
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("config.ini", addressInput.Text + ",");
+            File.AppendAllText("config.ini", intervalInput.Text);
+            File.AppendAllText("log.txt", "Updated config.ini" + Environment.NewLine);
+            heartbeatTimer.Stop();
+            heartbeatTimer.Interval = int.Parse(intervalInput.Text) * 1000;
+            heartbeatTimer.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            ShowInTaskbar = false;
+            e.Cancel = true;
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+        private void quit(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            ContextMenu cm = new ContextMenu();
+            cm.MenuItems.Add("Config", new EventHandler(NotifyIcon_DoubleClick));
+            cm.MenuItems.Add("Quit", new EventHandler(quit));
+            notifyIcon1.ContextMenu = cm;
+        }
+
+        private void Form1_FormShown(object sender, EventArgs e)
+        {
+            File.WriteAllText("log.txt", "Sucessfully intialised" + Environment.NewLine);
+            Boolean configExists = File.Exists("config.ini");
+            if (configExists)
+            {
+                try
+                {
+                    String config = File.ReadAllText("config.ini");
+                    String[] configValues = config.Split(',');
+                    File.AppendAllText("log.txt", "Loaded config.ini" + Environment.NewLine);
+                    addressInput.Text = configValues[0];
+                    intervalInput.Text = configValues[1];
+                    heartbeatTimer.Interval = int.Parse(configValues[1]) * 1000;
+                    heartbeatTimer.Start();
+                    this.Hide();
+                    ShowInTaskbar = false;
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+                    File.AppendAllText("log.txt", "Config found but was unreadable." + Environment.NewLine + "Please configure the address and interval");
+                    this.Show();
+                    ShowInTaskbar = true;
+                }
+            }
+            else
+            {
+                File.AppendAllText("log.txt", "Couldn't find a config" + Environment.NewLine + "Please configure the address and interval" + Environment.NewLine);
+                this.Show();
+                ShowInTaskbar = true;
+            }
+
+        
+        }
+
+        private void heartbeatTimer_Tick(object sender, EventArgs e)
+        {
+            File.AppendAllText("log.txt", "Sent heartbeat at " + DateTime.Now.ToString("h:mm:ss tt") + Environment.NewLine);
+        }
+    }
+
+}
